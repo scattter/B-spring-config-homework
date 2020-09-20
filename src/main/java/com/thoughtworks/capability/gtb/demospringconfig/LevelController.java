7eab7de78d26b3b14d6ac2306ffde697e2d3150a
scattter <413365742@qq.com>
2020-09-20 22:10:01 +0800

    [zk] p2s3

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/LevelController.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/LevelController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -4,23 +4,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class LevelControllerTest {
+public class LevelControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
     @Autowired
-    private MockMvc mockMvc;
+    LevelController levelController;
 
     @Test
-    void shouldReturnBasicLevel() throws Exception {
+    public void should_return_basic_when_levelNumber_is_less_1() throws Exception {
+        ReflectionTestUtils.setField(levelController,"levelNumber",-1);
         mockMvc.perform(get("/level"))
-                .andExpect(jsonPath("$", is("advanced")));
+                .andExpect(content().string("basic"));
+    }
+
+    @Test
+    public void should_return_basic_when_levelNumber_is_1() throws Exception {
+        ReflectionTestUtils.setField(levelController,"levelNumber",1);
+        mockMvc.perform(get("/level"))
+                .andExpect(content().string("advanced"));
+    }
+
+    @Test
+    public void should_return_basic_when_levelNumber_is_greater_1() throws Exception {
+        ReflectionTestUtils.setField(levelController,"levelNumber",2);
+        mockMvc.perform(get("/level"))
+                .andExpect(content().string("advanced"));
     }
 }
