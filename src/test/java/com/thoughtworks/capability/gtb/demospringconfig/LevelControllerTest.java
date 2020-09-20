dc3fb22ec56bb74a46fbeb0a53b6ddab5c61456c
scattter <413365742@qq.com>
2020-09-20 22:13:54 +0800

    [zk] p2s2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=1")
 class LevelControllerTest {
 
     @Autowired
